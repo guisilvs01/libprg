aa4238d9cf18c8897ed0eb03afea54e1f6e63546
gui <guilherme.r08@aluno.ifsc.edu.br>
2024-03-20 10:53:10 -0300

    ok

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -8,8 +8,23 @@ typedef struct {
 
 typedef enum { SUM, SUB } operation_t;
 
+int buscaLinear (lista* lista, int valor);
+int buscaBináriaiterativa (lista* lista, int valor);
+int buscaBinária recursiva (lista* lista, int valor, int inicio, int fim);
+
 result_t sub(double a, double b);
 result_t sum(double a, double b);
 result_t compute(double a, double b, operation_t op);
 
 #endif
+
+/*
+ * lista* criar lista (int capacidade, int* elementos, bool ordenada) ou (bool ordenada)
+ * void ou lista* ou int  inserir (lista* lista, int valor,)
+ * "retorno igual o de cima" remover (lista* lista, int valor
+ *
+ * int busca linear (lista* lista, int valor)
+ * int busca binária iterativa (lista* lista, int valor)
+ * int busca binária recursiva (lista* lista, int valor, int inicio, int fim)
+ *
+ * */
--- libprg/src/libprg/listaLinear.c
+++ libprg/src/libprg/listaLinear.c
@@ -1,3 +1,4 @@
-//
+
+
 // Created by aluno on 13/03/24.
 //
