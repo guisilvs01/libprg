07f12c28cee9ac05f60ab6a71913837a41916068
aluno <guilherme.r08@aluno.ifsc.edu.br>
2024-04-01 10:47:22 -0300

    get elementos

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -21,6 +21,7 @@ void inserirElemento(lista_t *lista, int elemento);
 int removerElemento(lista_t *lista, int elemento);
 int* getElementos(lista_t *lista);
 int getSize (lista_t *lista);
+void imprimirLista(lista_t *lista);
 
 //int buscaLinear (lista_t * lista, int valor);
 //int buscaBináriaiterativa (lista_t * lista, int valor);
